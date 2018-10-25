fa2754623a7c59d19e6963adfbb4483e34026d5d
Jeffrey Ede <jeffreyede1@outlook.com>
2018-10-25 15:21:04 +0100

    Improved comments, removed misc code

--- misc_py/NFFA-Europe_SEM_Dataset.py
+++ misc_py/NFFA-Europe_SEM_Dataset.py
@@ -3,6 +3,13 @@ import os
 import cv2
 from PIL import Image
 
+"""
+Script to automate the creation of 32x32, 64x64 and 96x96 versions of
+the NFFA-EUROPE 100% SEM dataset from 
+https://b2share.eudat.eu/records/f1aa0f5ad38c456eaf7b04d47a65af53
+Contact Jeffrey Ede (j.m.ede@warwick.ac.uk)
+"""
+
 parent = "Z:/Jeffrey-Ede/NFFA-EUROPE_SEM_Dataset/"
 
 parent32 = "Z:/Jeffrey-Ede/NFFA-EUROPE_SEM_Dataset/32x32/"
@@ -20,32 +27,12 @@ subs = ["Biological",
         "Powder",
         "Tips"]
 
-def scale0to1(img):
-    """Rescale image between 0 and 1"""
-
-    img = img.astype(np.float32)
-
-    min = np.min(img)
-    max = np.max(img)
-
-    if np.absolute(min-max) < 1.e-6:
-        img.fill(0.5)
-    else:
-        img = (img-min) / (max-min)
-
-    return img.astype(np.float32)
-
-def disp(img):
-    cv2.namedWindow('CV_Window', cv2.WINDOW_NORMAL)
-    cv2.imshow('CV_Window', scale0to1(img))
-    cv2.waitKey(0)
-    return
-
-#Load each file in NFFA-EUROPE SEM dataset and save as 32x32 and 96x96
+#Load each file in NFFA-EUROPE SEM dataset and save as 32x32, 64x64 and 96x96
 #after cropping the information boxes
 total_files = 0
 num_subs = len(subs)
-for i, sub in enumerate(subs):
+for i, sub in enumerate(subs): #Dataset is divided into 10 subsets...
+
     daughter = sub+"/"+sub+"/"
     dir = parent+daughter
     files = os.listdir(dir)
@@ -56,7 +43,8 @@ for i, sub in enumerate(subs):
 
     num_files = len(files)
     total_files += num_files
-    for j, file in enumerate(files):
+
+    for j, file in enumerate(files): #... that contain hundreds to thousands of files
         try:
             print("Subset {} of {}. File {} of {}.".format(i+1, num_subs, j+1, num_files))
 
@@ -75,7 +63,8 @@ for i, sub in enumerate(subs):
             img = img[:box_start_idx,:].astype(np.float32)
 
             def to_datasets(img, label):
-                #Save 32x32 and 96x96 variants
+                """Iterpolate and save 32x32, 64x64 and 96x96 variants"""
+
                 img32 = cv2.resize(img, (32,32), cv2.INTER_AREA)
                 img64 = cv2.resize(img, (64,64), cv2.INTER_AREA)
                 img96 = cv2.resize(img, (96,96), cv2.INTER_AREA)
@@ -84,6 +73,7 @@ for i, sub in enumerate(subs):
                 Image.fromarray( img32.astype(np.uint8) ).save( dir32+new_filename )
                 Image.fromarray( img64.astype(np.uint8) ).save( dir64+new_filename )
                 Image.fromarray( img96.astype(np.uint8) ).save( dir96+new_filename )
+
                 return
 
             to_datasets(img[:,:img.shape[0]], "left")
@@ -92,6 +82,5 @@ for i, sub in enumerate(subs):
             to_datasets(img[:,(img.shape[1]-img.shape[0]-1):], "right")
         except:
             continue
-
 
 print(total_files)
